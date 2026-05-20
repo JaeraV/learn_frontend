0a6bcfd8a888820b1490157d2294a4399cde6e0a
jaera <jaera.vllr@gmail.com>
2026-05-19 23:12:06 -1000

    add jsx syntax

--- Learn front end 2/react-course/src/App.jsx
+++ Learn front end 2/react-course/src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+  const name = 'John Doe';
+  const x = 10;
+  const y = 20;
+  const sum = x + y;
+  const names = ['Alice', 'Bob', 'Charlie'];
+  const loggedIn = true;
+
+  // if (loggedIn) {
+  // return <p>Welcome back</p>;
+  // }
 
 const App = () => {
   return (
-    <div className='text-5xl'>App</div>
+    <>
+      <div className='text-5xl'>App</div>
+      <p>Hello, {name}!</p>
+      <p>The sum of {x} and {y} is {sum}.</p>
+      <ul>
+        {names.map((name, index) => (
+          <li key={index}>{name}</li>
+        ))}
+      </ul>
+      {loggedIn ? <p>Hello Member</p> :""}
+    </>
   )
 }
 
