c6e2eeff814e2706d80eef7799dc7cb326019420
jaera <jaera.vllr@gmail.com>
2026-05-26 23:36:39 -1000

    change a tag to link tag in navbar.jsx

--- Learn front end 2/react-course/src/App.jsx
+++ Learn front end 2/react-course/src/App.jsx
@@ -2,11 +2,14 @@ import React from 'react'
 import {Route, createBrowserRouter, createRoutesFromElements, RouterProvider} from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import MainLayout from './Layouts/MainLayout'
+import JobsPage from './pages/JobsPage'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<MainLayout />}>
       <Route index element={<HomePage />} /> 
+      <Route path='/jobs' element={<JobsPage />} />
+
     </Route>
   )
 );
